31460d6277dc7b29fd64dbbb8cd1f607e8e96ab8
MahmoodAhmed95 <Mahmood1995ahmed@gmail.com>
2025-02-11 14:03:15 +0300

    adding basic login functionality for testing and adding the hard coded texts in the strings.xml for best practice

--- app/src/main/java/com/example/eazypay/MainActivity.java
+++ app/src/main/java/com/example/eazypay/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.eazypay;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,20 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        TextView username = (TextView) findViewById(R.id.emailInp);
+        TextView password = (TextView) findViewById(R.id.passwordInp);
+        MaterialButton loginBtn = (MaterialButton) findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+                    System.out.println("correct login");
+                }else{
+                    System.out.println("invalid login");
+                }
+            }
+
         });
     }
 }
